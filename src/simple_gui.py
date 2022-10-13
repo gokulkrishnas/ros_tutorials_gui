c435ac1ce7e8f736bde5b1895f2aeaabc8bf1d64
gokulks <48028723+gokulkrishnas@users.noreply.github.com>
2022-10-13 22:13:48 +0530

    icon addition

--- src/simple_gui.py
+++ src/simple_gui.py
@@ -1,10 +1,9 @@
 from unicodedata import name
 import rospy
-from kivy.config import Config
-Config.set('kivy','keyboard_mode','systemanddock')
-from kivy.app import App
 from kivymd.app import MDApp
 from kivy.lang import Builder
+from kivymd.uix.screen import MDScreen
+from kivymd.uix.button import MDRectangleFlatIconButton
 
 
 class BlahApp(MDApp):
@@ -15,6 +14,18 @@ class BlahApp(MDApp):
         self.screen = Builder.load_file('/home/gokulkrishna_s/gui_ws/src/ros_tutorials_gui/src/ros_gui.kv')
     
     def build(self):
+        self.theme_cls.theme_style = "Dark"
+        self.theme_cls.primary_palette = "Green"
+        return (
+            MDScreen(
+                MDRectangleFlatIconButton(
+                    text="MDRectangleFlatIconButton",
+                    icon="language-python",
+                    line_color=(1, 1, 1, 1),
+                    pos_hint={"center_x": .5, "center_y": .5},
+                )
+            )
+        )
         return self.screen
 
 if __name__=='__main__':
