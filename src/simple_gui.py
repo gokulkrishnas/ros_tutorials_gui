ee4f3d8e1510988d148f5e62edd750bcb14b402d
gokulks <48028723+gokulkrishnas@users.noreply.github.com>
2022-10-13 23:08:48 +0530

    ros addition

--- src/simple_gui.py
+++ src/simple_gui.py
@@ -1,9 +1,12 @@
+#!/usr/bin/env python3
+
 from unicodedata import name
 import rospy
 from kivymd.app import MDApp
 from kivy.lang import Builder
 from kivymd.uix.screen import MDScreen
-from kivymd.uix.button import MDRectangleFlatIconButton
+from kivymd.uix.button import MDRoundFlatIconButton
+from kivymd.uix.menu import MDDropdownMenu
 
 
 class BlahApp(MDApp):
@@ -14,21 +17,21 @@ class BlahApp(MDApp):
         self.screen = Builder.load_file('/home/gokulkrishna_s/gui_ws/src/ros_tutorials_gui/src/ros_gui.kv')
     
     def build(self):
-        self.theme_cls.theme_style = "Dark"
-        self.theme_cls.primary_palette = "Green"
+        self.theme_cls.theme_style = "Light"
+        self.theme_cls.primary_palette = "Red"
         return (
             MDScreen(
-                MDRectangleFlatIconButton(
-                    text="MDRectangleFlatIconButton",
+                MDRoundFlatIconButton(
+                    text="Click here",
                     icon="language-python",
-                    line_color=(1, 1, 1, 1),
-                    pos_hint={"center_x": .5, "center_y": .5},
+                    line_color=(0.25, 0.25, 0.25, 0.25),
+                    pos_hint={"center_x": .25, "center_y": .25},
                 )
             )
         )
-        return self.screen
+        #return self.screen
 
 if __name__=='__main__':
-    # rospy.init_node('simple_gui',anonymous=True)
+    rospy.init_node('simple_gui',anonymous=True)
 
     BlahApp().run()
