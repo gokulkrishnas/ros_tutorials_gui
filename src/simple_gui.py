981917f53f5f8a28ccd81bb0fc6d557cca0be16a
gokulks <48028723+gokulkrishnas@users.noreply.github.com>
2022-10-17 19:32:11 +0530

    publish true when button pressed

--- src/simple_gui.py
+++ src/simple_gui.py
@@ -2,24 +2,31 @@
 
 from unicodedata import name
 import rospy
+from std_msgs.msg import Bool
 from kivymd.app import MDApp
 from kivy.lang import Builder
-from kivymd.uix.screen import MDScreen
-from kivymd.uix.button import MDRoundFlatIconButton
 
 
 class BlahApp(MDApp):
 
+    # defult code 
     def __init__(self,**kwargs):
         super().__init__(**kwargs)
-
         self.screen = Builder.load_file('/home/gokulkrishna_s/gui_ws/src/ros_tutorials_gui/src/ros_gui.kv')
     
+# used to display UI components in .kv file    
     def build(self):
-        
         return self.screen
 
-if __name__=='__main__':
-    rospy.init_node('simple_gui',anonymous=True)
+# used to publish true when button is pressed
+    def my_function(self,*args):
+        print("Button pressed")
+        msg = True
+        pub.publish(msg)
 
+
+if __name__=='__main__':
+
+    pub=rospy.Publisher('/button',Bool,queue_size=1)
+    rospy.init_node('simple_gui',anonymous=True)
     BlahApp().run()
