82dc208c90b2e652d1516b45c887bf1d75037ad5
gokulks <48028723+gokulkrishnas@users.noreply.github.com>
2022-10-17 18:59:52 +0530

    adding label and button through .kv file

--- src/simple_gui.py
+++ src/simple_gui.py
@@ -16,20 +16,8 @@ class BlahApp(MDApp):
         self.screen = Builder.load_file('/home/gokulkrishna_s/gui_ws/src/ros_tutorials_gui/src/ros_gui.kv')
     
     def build(self):
-        self.theme_cls.theme_style = "Light"
-        self.theme_cls.primary_palette = "Green"
-        return (
-            MDScreen(
-                MDRoundFlatIconButton(
-                    text="Click here",
-                    icon="language-python",
-                    line_color=(0.25, 0.25, 0.25, 0.25),
-                    size_hint=(0.15,0.15),
-                    pos_hint={"center_x": .25, "center_y": .25},
-                )
-            )
-        )
-        #return self.screen
+        
+        return self.screen
 
 if __name__=='__main__':
     rospy.init_node('simple_gui',anonymous=True)
